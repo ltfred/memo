ee958ec7b5226042c0511dd1639475f438871ba8
ltfred <ltfred@163.com>
2023-05-24 15:15:13 +0800

    feat: clear all records confirm

--- command/clear.go
+++ command/clear.go
@@ -2,6 +2,7 @@ package command
 
 import (
 	"github.com/ltfred/memo/pkg/parser"
+	"github.com/manifoldco/promptui"
 	"github.com/urfave/cli"
 )
 
@@ -17,6 +18,12 @@ func init() {
 }
 
 func clearAction(c *cli.Context) error {
+	prompt := promptui.Prompt{Label: "clear all records", IsConfirm: true}
+	_, err := prompt.Run()
+	if err != nil {
+		return err
+	}
+
 	par := parser.Parser{}
 	return par.Clear()
 }
